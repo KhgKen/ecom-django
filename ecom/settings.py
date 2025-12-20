d2e7515e0abb6707a8a5403ca83fbaf677b20264
khang <khangtm2739@ut.edu.vn>
2025-12-20 14:11:51 +0700

    change address

--- ecom/settings.py
+++ ecom/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = 'django-insecure-m!n4_^hjs*vl5e_5ne9r*7rlv76$(^*0zc@(@d8gdrbc)n2(n3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecom-django-production-957a.up.railway.app','https://ecom-django-production-957a.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://ecom-django-production-957a.up.railway.app']
+ALLOWED_HOSTS = ['ecom-django-production-587c.up.railway.app','https://ecom-django-production-587c.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://ecom-django-production-587c.up.railway.app']
 
 
 # Application definition
